2a21d82ef52c0a09aa3bdbf1132c17d30569f84a
User <User@DESKTOP-6JPNDEN.lan>
2024-01-03 21:03:40 -0500

    DemoC

---
+++ src/C/DemoC.java
@@ -0,0 +1,14 @@
+package C;
+
+public class DemoC {
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("GitHub");
+		System.out.println("GitHub Push");
+		System.out.println("GitHub Day 1");
+		
+	}
+
+}
